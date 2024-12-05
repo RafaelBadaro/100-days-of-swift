499a7e81e6f99f685137d491ce2d9a0fdf798c6b
Rafael Badaro <rafabadaro.15@gmail.com>
2024-12-04 21:24:46 -0300

    Jogo guess the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Rafael Badaró on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
