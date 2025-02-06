0d1a7fd7b589b80e4ce4bb8e724d4fe66963645f
Rafael Badaro <rafabadaro.15@gmail.com>
2025-02-05 21:53:13 -0300

    Day 36 - inicio do iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rafael Badaró on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
