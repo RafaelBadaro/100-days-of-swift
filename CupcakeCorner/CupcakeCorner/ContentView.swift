bbe197d9c0ecc41a7884e406df6739e63cfaac62
Rafael Badaro <rafabadaro.15@gmail.com>
2025-04-14 20:21:33 -0300

    Day 50! Iniciando o Cupcake corner e aprendendo sobre haptics

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,29 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var order = Order()
     
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-        
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account...")
+   
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)",
+                            value: $order.quantity, in: 3...20)
                 }
+                
+                Section {
+                    Toggle("Any special requests?",
+                           isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
+                }
+                
             }
-            .disabled(disableForm)
+            .navigationTitle("Cupcake Corner")
             
         }
+        
     }
 }
 
@@ -40,6 +56,137 @@ struct ContentView: View {
 
 
 /*
+ 
+ 
+ ------------------
+ 
+ import CoreHaptics
+ import SwiftUI
+
+ struct ContentView: View {
+     @State private var engine: CHHapticEngine?
+     
+     var body: some View {
+         Button("Play haptic", action: complexSuccess)
+             .onAppear(perform: prepareHaptics)
+     }
+     
+     
+     func prepareHaptics() {
+         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+         
+         do {
+             engine = try CHHapticEngine()
+             try engine?.start()
+         } catch {
+             print("Failed to create haptic engine: \(error.localizedDescription)")
+         }
+     }
+     
+     func complexSuccess() {
+         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+         
+         var events = [CHHapticEvent]()
+         
+         for i in stride(from: 0, to: 1, by: 0.1) {
+             let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+             let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+             let event = CHHapticEvent(eventType: .hapticTransient,
+                                       parameters: [intensity, sharpness],
+                                       relativeTime: i)
+             events.append(event)
+         }
+         
+         for i in stride(from: 0, to: 1, by: 0.1) {
+             let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+             let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+             let event = CHHapticEvent(eventType: .hapticTransient,
+                                       parameters: [intensity, sharpness],
+                                       relativeTime: 1 + i)
+             events.append(event)
+         }
+         
+         do {
+             let pattern = try CHHapticPattern(events: events, parameters: [])
+             let player = try engine?.makePlayer(with: pattern)
+             try player?.start(atTime: 0)
+         } catch {
+             print("Failed to play pattern: \(error.localizedDescription)")
+         }
+         
+     }
+ }
+
+ 
+ ------------------
+ 
+ struct ContentView: View {
+     @State private var counter = 0
+     
+     var body: some View {
+         Button("Tap count: \(counter)") {
+             counter += 1
+         }
+         .sensoryFeedback(.impact(weight: .heavy, intensity: 1),
+                          trigger: counter)
+     }
+ }
+ 
+ ------------------
+ 
+ @Observable
+ class User: Codable {
+     // Sem o coding keys: {"_name":"rafael","_$observationRegistrar":{}}
+     // Com o coding keys: {"name":"rafael"}
+     // Ele faz com que @Observable não printe algo como "_name"
+     enum CodingKeys: String, CodingKey {
+         case _name = "name"
+     }
+     var name = "rafael"
+ }
+
+ struct ContentView: View {
+     var body: some View {
+         Button ("Encode", action: encodeUser)
+     }
+     
+     func encodeUser() {
+         let data = try! JSONEncoder().encode(User())
+         let str = String(decoding: data, as: UTF8.self)
+         print(str)
+     }
+ }
+ 
+ ------------------
+ 
+ struct ContentView: View {
+     
+     @State private var username = ""
+     @State private var email = ""
+     
+     var disableForm: Bool {
+         username.count < 5 || email.count < 5
+     }
+         
+     var body: some View {
+         Form {
+             Section {
+                 TextField("Username", text: $username)
+                 TextField("Email", text: $email)
+             }
+             
+             Section {
+                 Button("Create account") {
+                     print("Creating account...")
+                 }
+             }
+             .disabled(disableForm)
+             
+         }
+     }
+ }
+ 
+ ------------------
  
  struct ContentView: View {
      
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Rafael Badaró on 14/04/25.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var extraFrosting = false
+    var addSprinkles = false
+}
