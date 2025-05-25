b27bc3c0d8a164b89f876b4da1713ae1c87407b9
Rafael Badaro <rafabadaro.15@gmail.com>
2025-05-25 13:19:58 -0300

    Day 60 done

--- day60-challenge-fetch-json/day60-challenge-fetch-json/DetailView.swift
+++ day60-challenge-fetch-json/day60-challenge-fetch-json/DetailView.swift
@@ -10,10 +10,33 @@ import SwiftUI
 struct DetailView: View {
     let user: User
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section("Info") {
+                Text("Age: \(user.age)")
+                Text("Company: \(user.age)")
+                Text("Email: \(user.email)")
+                Text("Address: \(user.address)")
+                Text("About: \(user.about)")
+                Text("Registered: \(user.registered, format: .dateTime)")
+
+            }
+            
+            Section ("Tags") {
+                ForEach(user.tags, id: \.self) { tag in
+                    Text(tag)
+                }
+            }
+
+            Section ("Friends") {
+                ForEach(user.friends) { friend in
+                    Text(friend.name)
+                }
+            }
+        }
+        .navigationTitle(user.name)
     }
 }
 
-//#Preview {
-//    DetailView()
-//}
+#Preview {
+    DetailView(user: User.sampleData[0])
+}
--- day60-challenge-fetch-json/day60-challenge-fetch-json/User.swift
+++ day60-challenge-fetch-json/day60-challenge-fetch-json/User.swift
@@ -20,3 +20,24 @@ struct User: Identifiable, Codable, Hashable {
     var tags: [String]
     var friends: [Friend]
 }
+
+
+extension User {
+    static let sampleData: [User] = [
+        User(
+            id: "50a48fa3-2c0f-4397-ac50-64da464f9954",
+            isActive: false,
+            name: "Alford Rodriguez",
+            age: 21,
+            company: "Imkan",
+            email: "alfordrodriguez@imkan.com",
+            address: "907 Nelson Street, Cotopaxi, South Dakota, 5913",
+            about: "Occaecat consequat elit aliquip magna laboris dolore laboris sunt officia adipisicing reprehenderit sunt. Do in proident consectetur labore. Laboris pariatur quis incididunt nostrud labore ad cillum veniam ipsum ullamco. Dolore laborum commodo veniam nisi. Eu ullamco cillum ex nostrud fugiat eu consequat enim cupidatat. Non incididunt fugiat cupidatat reprehenderit nostrud eiusmod eu sit minim do amet qui cupidatat. Elit aliquip nisi ea veniam proident dolore exercitation irure est deserunt.",
+            registered: ISO8601DateFormatter().date(from: "2015-11-10T01:47:18-00:00")!,
+            tags: ["cillum", "consequat", "deserunt", "nostrud", "eiusmod", "minim", "tempor"],
+            friends: [
+                Friend(id: "91b5be3d-9a19-4ac2-b2ce-89cc41884ed0", name: "Hawkins Patel")
+            ]
+        )
+    ]
+}
