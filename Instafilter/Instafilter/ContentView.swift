acb017517596187942b53c87ae911d683ba6c36a
Rafael Badaro <rafabadaro.15@gmail.com>
2025-05-29 17:12:11 -0300

    Day 62 aula 3

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
-        VStack {
-            Text("Hello world!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
+        Button("Hello, World!") {
+            showingConfirmation.toggle()
         }
-//        .onChange(of: blurAmount) { oldValue, newValue in
-//            print("New value is \(newValue)")
-//        }
-        .onChange(of: blurAmount) {
-            
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+        } message: {
+            Text("Select a new color")
         }
     }
 }
