b29c884945a5476eed7ec513e512d4b1cd097f39
Rafael Badaro <rafabadaro.15@gmail.com>
2025-05-30 10:06:03 -0300

    Day 63 aula 2, ContentUnavailableView

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,53 +5,25 @@
 //  Created by Rafael Badaró on 29/05/25.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    
-    
     var body: some View {
-        VStack{
-            image?
-                .resizable()
-                .scaledToFit()
+//        ContentUnavailableView("No snippets",
+//                               systemImage: "swift",
+//        description: Text("You don't have any saved snippets yet"))
+        ContentUnavailableView {
+            Label("No snippets",  systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet")
+        } actions: {
+            Button("Create snippets") {
+                
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .onAppear(perform: loadImage)
     }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.crystallize()
-        
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
 
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
-    }
 }
 
 #Preview {
