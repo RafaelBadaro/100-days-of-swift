9e16a227c3f21806f855d8425f84c78e8e69de47
Rafael Badaro <rafabadaro.15@gmail.com>
2025-06-05 10:19:42 -0300

    Day 65 aula 2

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -6,26 +6,31 @@
 //
 
 import SwiftUI
-import StoreKit
+import PhotosUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture",
-                                           systemImage: "photo.badge.plus",
-                                           description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture",
+                                               systemImage: "photo.badge.plus",
+                                               description: Text("Tap to import a photo"))
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -46,12 +51,21 @@ struct ContentView: View {
             .navigationTitle("Instafilter")
         }
         
-        
-        
     }
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            
+            
+        }
     }
 }
 
