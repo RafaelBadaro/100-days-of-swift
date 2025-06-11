973bb5fd36cfe990e26c764ca70ec9b536a11b6b
Rafael Badaro <rafabadaro.15@gmail.com>
2025-06-11 17:04:10 -0300

    Day 68, aula 2 - FileManager extension

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,32 +7,82 @@
 
 import SwiftUI
 
-struct User: Comparable, Identifiable {
-    let id = UUID()
-    var firstname: String
-    var lastName: String
+extension FileManager {
     
-    // Sim a func chama "<"
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
+    func read (_ fileName: String) -> String {
+        let url = URL.documentsDirectory.appendingPathComponent(fileName)
+        
+        guard let data = try? String(contentsOf: url, encoding: .utf8) else {
+            fatalError()
+        }
+    
+        return data
+    }
+    
+    func write(_ fileName: String,_ dataToWrite: Data){
+        let url = URL.documentsDirectory.appendingPathComponent(fileName)
+        try? dataToWrite.write(to: url, options: [.atomic, .completeFileProtection])
     }
 }
 
 struct ContentView: View {
-    
-    let users = [
-        User(firstname: "Arnold", lastName: "Rimmer"),
-        User(firstname: "Kristine", lastName: "Kochanski"),
-        User(firstname: "David", lastName: "Lister"),
-    ].sorted()
+  
     
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstname)")
+        Button("Read and Write") {
+            let data = Data("Test Message".utf8)
+            FileManager.default.write("message.txt", data)
+            let input: String = FileManager.default.read("message.txt")
+            print(input)
         }
     }
+    
+    
 }
 
 #Preview {
     ContentView()
 }
+
+/***
+ 
+ Aula 1:
+ 
+ struct User: Comparable, Identifiable {
+     let id = UUID()
+     var firstname: String
+     var lastName: String
+     
+     // Sim a func chama "<"
+     static func < (lhs: User, rhs: User) -> Bool {
+         lhs.lastName < rhs.lastName
+     }
+ }
+
+ struct ContentView: View {
+     
+     let users = [
+         User(firstname: "Arnold", lastName: "Rimmer"),
+         User(firstname: "Kristine", lastName: "Kochanski"),
+         User(firstname: "David", lastName: "Lister"),
+     ].sorted()
+     
+     var body: some View {
+         List(users) { user in
+             Text("\(user.lastName), \(user.firstname)")
+         }
+     }
+ }
+ 
+ 
+ --------
+ 
+ Aula 2:
+ 
+ 
+ 
+ --------
+ 
+ Aula 3:
+ 
+ */
