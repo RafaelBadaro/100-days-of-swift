4e3d227e4ca40ff9c2e47f93fccd96d78f01a46f
Rafael Badaro <rafabadaro.15@gmail.com>
2025-06-16 18:38:04 -0300

    Day 71, aula 1 - fetch dados da wikipedia

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+enum LoadingState {
+    case loading, loaded, failed
+}
+
 struct EditView: View {
     @Environment(\.dismiss) var dismiss
     
@@ -17,13 +21,14 @@ struct EditView: View {
     
     var onSave: (Location) -> Void
     
+    @State private var loadingState: LoadingState = .loading
+    @State private var pages = [Page]()
+    
     var body: some View {
         NavigationStack {
             Form {
-                Section {
-                    TextField("Place name", text: $name)
-                    TextField("Description", text: $description)
-                }
+                PlaceDetailsSection(name: $name, description: $description)
+                NearbyPagesSection(loadingState: loadingState, pages: pages)
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -37,6 +42,9 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     
@@ -47,7 +55,82 @@ struct EditView: View {
        _name = State(wrappedValue: location.name)
        _description = State(wrappedValue: location.description)
     }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
+    }
 }
+
+struct PlaceDetailsSection: View {
+    @Binding var name: String
+    @Binding var description: String
+
+    var body: some View {
+        Section {
+            TextField("Place name", text: $name)
+            TextField("Description", text: $description)
+        }
+    }
+}
+
+
+struct NearbyPagesSection: View {
+    let loadingState: LoadingState
+    let pages: [Page]
+
+    var body: some View {
+        Section("Nearby...") {
+            switch loadingState {
+            case .loading:
+                Text("Loading...")
+            case .loaded:
+                PagesListView(pages: pages)
+            case .failed:
+                Text("Please try again later.")
+            }
+        }
+    }
+}
+
+struct PagesListView: View {
+    let pages: [Page]
+
+    var body: some View {
+        ForEach(pages, id: \.pageid) { page in
+            PageRow(page: page)
+        }
+    }
+}
+
+struct PageRow: View {
+    let page: Page
+
+    var body: some View {
+        HStack(alignment: .top, spacing: 0) {
+            Text(page.title)
+                .font(.headline)
+            Text(": ")
+            Text("Page description here")
+                .italic()
+        }
+    }
+}
+
+
 
 #Preview {
     EditView(location: .example) { _ in }
