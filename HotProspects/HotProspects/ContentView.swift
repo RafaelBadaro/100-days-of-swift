2026dc0b00acfec95633228d00cee11567400ed5
Rafael Badaro <rafabadaro.15@gmail.com>
2025-07-24 19:26:45 -0300

    Day 79 - aula 3, dia finalizado

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    let users = ["User 1", "User 2"]
-    @State private var selection = Set<String>()
+    @State private var selectedTab = "One"
     
     var body: some View {
-        List(users, id: \.self, selection: $selection) { user in
-            Text(user)
+        // Dica: a TabView prefere ser a "parent" view e as views dentro dela com a NavigationStack as necessary
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                    Label("One", systemImage: "star")
+            }.tag("One")
+            
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+            }.tag("Two")
         }
-        
-        if !selection.isEmpty {
-            Text("You selected \(selection.formatted())")
-        }
-        
-        EditButton()
     }
 }
 
@@ -29,15 +33,59 @@ struct ContentView: View {
 }
 
 /**
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  Day 79
  
  Aula 3:
+ struct ContentView: View {
+     @State private var selectedTab = "One"
+     
+     var body: some View {
+         // Dica: a TabView prefere ser a "parent" view e as views dentro dela com a NavigationStack as necessary
+         TabView(selection: $selectedTab) {
+             Button("Show Tab 2") {
+                 selectedTab = "Two"
+             }
+             .tabItem {
+                     Label("One", systemImage: "star")
+             }.tag("One")
+             
+             Text("Tab 2")
+                 .tabItem {
+                     Label("Two", systemImage: "circle")
+             }.tag("Two")
+         }
+     }
+ }
  
- 
+ --------
  Aula 2:
  
  
- 
+ struct ContentView: View {
+     let users = ["User 1", "User 2"]
+     @State private var selection = Set<String>()
+     
+     var body: some View {
+         List(users, id: \.self, selection: $selection) { user in
+             Text(user)
+         }
+         
+         if !selection.isEmpty {
+             Text("You selected \(selection.formatted())")
+         }
+         
+         EditButton()
+     }
+ }
  
  
  
