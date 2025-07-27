277ae66839651fb60914d4a1a38a7880f968378b
Rafael Badaro <rafabadaro.15@gmail.com>
2025-07-27 16:40:48 -0300

    Day 81 - aula 3 - usando o Swift Package manager

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,35 +6,10 @@
 //
 
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Request permission") {
-                UNUserNotificationCenter.current()
-                    .requestAuthorization(options: [.alert, .badge]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
     }
 }
 
@@ -105,6 +80,23 @@ struct ContentView: View {
  
  --------
  Aula 3:
+ 
+ import SamplePackage
+ import SwiftUI
+
+ struct ContentView: View {
+     let possibleNumbers = 1...60
+     
+     var results: String {
+         let selected = possibleNumbers.random(7).sorted()
+         let strings = selected.map(String.init)
+         return strings.formatted()
+     }
+     
+     var body: some View {
+         Text(results)
+     }
+ }
  
  
  
