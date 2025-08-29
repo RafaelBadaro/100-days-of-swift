404d936da7e211ac1b723690da456ccab9d326d8
Rafael Badaro <rafabadaro.15@gmail.com>
2025-08-29 07:13:11 -0300

    Day 92 - aula 2,3 e 4 - adicionando anotacoes das aulas

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -25,6 +25,67 @@ struct ContentView: View {
 
 /**
  
+ Day 92 - aulas 2,3,4
+ 
+ Aula 2 - ele mostrou o alignmentGuide, basicamente ele faz o override do alinhamento
+ no ex abaixo, "quando receber o .leading, retornar o .trailing (tem teoria pode ser qualquer coisa)
+ 
+ VStack(alignment: .leading) {
+     Text("Hello, world!")
+         .alignmentGuide(.leading) { d in d[.trailing] }
+     Text("This is a longer line of text")
+ }
+ 
+ Aula 3 - acheio meio confuso, mas a memsa ideia da aula 2, ele criou uma extension pra ser um "modelo" de alignment, no caso utiliza o .top
+ E depois ele fez o override disso de novo com o .alignmentGuide
+ 
+ extension VerticalAlignment {
+     struct MidAccountAndName: AlignmentID {
+         static func defaultValue(in context: ViewDimensions) -> CGFloat {
+             context[.top]
+         }
+     }
+
+     static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+ }
+ 
+ HStack(alignment: .midAccountAndName) {
+     VStack {
+         Text("@twostraws")
+             .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+         Image(.paulHudson)
+             .resizable()
+             .frame(width: 64, height: 64)
+     }
+
+     VStack {
+         Text("Full name:")
+         Text("PAUL HUDSON")
+             .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+             .font(.largeTitle)
+     }
+ }
+ 
+ Aula 4 - Ele mostrou a importancia da ordem dos modifiers utilizando o codigo abaixo:
+ 
+ Text("Hello, world!")
+     .background(.red)
+     .position(x: 100, y: 100)
+ 
+ Text("Hello, world!")
+     .position(x: 100, y: 100)
+     .background(.red)
+
+ Text("Hello, world!")
+     .offset(x: 100, y: 100)
+     .background(.red)
+ 
+ Text("Hello, world!")
+     .background(.red)
+     .offset(x: 100, y: 100)
+ 
+ 
+ 
  Day 92 - aula 1
  
  Os modifiers de uma View criam uma nova View
