af8a0ab3ffef32320970dc2afd1ad9a06f29831f
Rafael Badaro <rafabadaro.15@gmail.com>
2025-09-06 16:58:08 -0300

    Day 96 - aula 2

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,16 +7,58 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+
+    
     var body: some View {
-        NavigationSplitView {
-            Text("primary")
-        } detail: {
-            Text("content")
-        }
+   
     }
 }
 
 #Preview {
-    ContentView() 
+    ContentView()
 }
+
+/**
+ 
+ Day 96 - aula 3
+ 
+ ------
+ Day 96 - aula 2
+ 
+ struct User: Identifiable {
+     var id = "Test"
+ }
+ 
+ struct ContentView: View {
+     @State private var selectedUser: User? = nil
+     
+     var body: some View {
+         Button("Tap me") {
+             selectedUser = User()
+         }
+         .sheet(item: $selectedUser) { unwrappedUser in
+             Text(unwrappedUser.id)
+         }
+     }
+ }
+ 
+ struct ContentView: View {
+     @State private var selectedUser: User? = nil
+     @State private var isShowingAlert = false
+     
+     var body: some View {
+         Button("Tap me") {
+             selectedUser = User()
+             isShowingAlert = true
+         }
+         .alert("Hello", isPresented: $isShowingAlert, presenting: selectedUser) { unwrappedUser in
+             Text(unwrappedUser.id)
+         }
+     }
+ }
+ 
+ 
+ 
+ */
