4333d6cfdcf5bc12eaa72de362adc96bf21747ed
Rafael Badaro <rafabadaro.15@gmail.com>
2025-09-08 17:08:01 -0300

    Day 98 - aula 4 - criando a Favorites class - day finalizado!

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     @State private var searchText = ""
+    @State private var favorites = Favorites()
     
     var filteredResorts: [Resort] {
         if searchText.isEmpty {
@@ -42,6 +43,13 @@ struct ContentView: View {
                             Text("\(resort.runs) runs")
                                 .foregroundStyle(.secondary)
                         }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundStyle(.red)
+                        }
                     }
                 }
             }
@@ -53,6 +61,7 @@ struct ContentView: View {
         } detail: {
             WelcomeView()
         }
+        .environment(favorites)
     }
 }
 
--- SnowSeeker/SnowSeeker/Resort/Views/ResortView.swift
+++ SnowSeeker/SnowSeeker/Resort/Views/ResortView.swift
@@ -12,6 +12,7 @@ struct ResortView: View {
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    @Environment(Favorites.self) var favorites
     
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
@@ -19,44 +20,15 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                resortDecorativeImageView
+                detailViews
+                descriptionAndFacilityViews
                 
-                HStack {
-                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
-                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
-                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
-                    } else {
-                        ResortDetailsView(resort: resort)
-                        SkiDetailsView(resort: resort)
-                    }
+                Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                    favorites.contains(resort) ? favorites.remove(resort) : favorites.add(resort)
                 }
-                .padding(.vertical)
-                .background(.primary.opacity(0.1))
-                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
-                
-                Group {
-                    Text(resort.description)
-                        .padding(.vertical)
-                    
-                    Text("Facilities")
-                        .font(.headline)
-                    
-                    HStack {
-                        ForEach(resort.facilityTypes) { facility in
-                            Button {
-                                selectedFacility = facility
-                                showingFacility = true
-                            } label: {
-                                facility.icon
-                                    .font(.title)
-                            }
-                        }
-                    }
-                    .padding(.vertical)
-                }
-                .padding(.horizontal)
+                .buttonStyle(.borderedProminent)
+                .padding()
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
@@ -71,6 +43,58 @@ struct ResortView: View {
     }
 }
 
+private extension ResortView {
+    private var resortDecorativeImageView: some View {
+        Image(decorative: resort.id)
+            .resizable()
+            .scaledToFit()
+    }
+    
+    private var detailViews: some View {
+        HStack {
+            if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+            } else {
+                ResortDetailsView(resort: resort)
+                SkiDetailsView(resort: resort)
+            }
+        }
+        .padding(.vertical)
+        .background(.primary.opacity(0.1))
+        .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+    }
+    
+    private var descriptionAndFacilityViews: some View {
+        Group {
+            Text(resort.description)
+                .padding(.vertical)
+            
+            Text("Facilities")
+                .font(.headline)
+            facilitiesView
+        }
+        .padding(.horizontal)
+    }
+    
+    private var facilitiesView: some View {
+        HStack {
+            ForEach(resort.facilityTypes) { facility in
+                Button {
+                    selectedFacility = facility
+                    showingFacility = true
+                } label: {
+                    facility.icon
+                        .font(.title)
+                }
+            }
+        }
+        .padding(.vertical)
+    }
+    
+}
+
 #Preview {
     ResortView(resort: .example)
+        .environment(Favorites())
 }
