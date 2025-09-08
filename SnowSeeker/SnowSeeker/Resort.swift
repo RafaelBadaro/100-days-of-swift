3e6c9d4db13c9ae67822fff13c400a30d2099ce1
Rafael Badaro <rafabadaro.15@gmail.com>
2025-09-08 15:28:35 -0300

    Day 97 - aula 1 - incio do projeto!

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,26 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Rafael Badaró on 08/09/25.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
+
